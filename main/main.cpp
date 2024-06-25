6b9b28e4d2f5b61d5ed4546585922fb6ab706217
niliha <75397148+niliha@users.noreply.github.com>
2024-06-25 12:05:11 +0200

    Add final changes

--- main/main.cpp
+++ main/main.cpp
@@ -1,37 +1,57 @@
 #include "Arduino.h"
 #include "DFRobotDFPlayerMini.h"
-#include "esp_log.h"
+#include <esp_log.h>
+#include <esp_sleep.h>
+#include <driver/rtc_io.h>
 
 constexpr int MOSFET_PIN = 6;
+constexpr int GATE_DRIVER_PIN = 3;
 constexpr int BLOW_FOLDER_NUM = 1;
 constexpr int HEINZELMANN_FOLDER_NUM = 2;
-constexpr int VOLUME_PERCENT = 50;
+constexpr int VOLUME_PERCENT = 60;
+constexpr gpio_num_t WAKEUP_PIN = GPIO_NUM_2;
 
 RTC_DATA_ATTR int bootCount = 0;
 DFRobotDFPlayerMini player;
 
+constexpr char *TAG = "main";
+
 void enterDeepSleep()
 {
-    // Configure pull-up resistor on wakeup pin that stays enabled during deep sleep
+    ESP_LOGI(TAG, "Enabled pull-up resistor on wakeup pin during deep sleep");
     esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
     rtc_gpio_pulldown_dis(WAKEUP_PIN);
     rtc_gpio_pullup_en(WAKEUP_PIN);
 
-    ESP_LOGI("Entering deep sleep...");
+    ESP_LOGI(TAG, "Turning off power supply for MP3 player...");
+    pinMode(GATE_DRIVER_PIN, OUTPUT);
+    digitalWrite(GATE_DRIVER_PIN, HIGH);
+    rtc_gpio_hold_en((gpio_num_t)GATE_DRIVER_PIN); // Required to hold pin state during deep sleep
+
+    ESP_LOGI(TAG, "Entering deep sleep...");
     esp_sleep_enable_ext1_wakeup(1ULL << WAKEUP_PIN, ESP_EXT1_WAKEUP_ANY_LOW);
     esp_deep_sleep_start();
 }
 
 void initializePlayer()
 {
-    ESP_LOGI("Initializing DFPlayer...");
+    ESP_LOGI(TAG, "Turning on power supply for MP3 player...");
+    rtc_gpio_hold_dis((gpio_num_t)GATE_DRIVER_PIN); // Required to change pin state after deep sleep
+    pinMode(GATE_DRIVER_PIN, OUTPUT);
+    digitalWrite(GATE_DRIVER_PIN, LOW);
+
+    ESP_LOGI(TAG, "Initializing MP3 player...");
     if (!player.begin(Serial1, /*isACK = */ true, /*doReset = */ true))
     {
-        ESP_LOGE("Could not initialize DFPlayer. Restarting...");
+        ESP_LOGE(TAG, "Could not initialize DFPlayer. Restarting...");
         ESP.restart();
     }
+    ESP_LOGI(TAG, "Successfully initialized MP3 player");
 
+    ESP_LOGI(TAG, "Setting volume to %d%%...", VOLUME_PERCENT);
     player.volume(map(VOLUME_PERCENT, 0, 100, 0, 30));
+    ESP_LOGI(TAG, "Performing dummy call...");
+    player.readFolderCounts(); // For some reason a dummy read is required, otherwise the next command will fail
 }
 
 void awaitPlaybackFinished(int timeout_seconds)
@@ -39,21 +59,21 @@ void awaitPlaybackFinished(int timeout_seconds)
     int timeoutMillis = timeout_seconds * 1000;
     auto startMillis = millis();
 
-    ESP_LOGI("Waiting for track to finish...");
+    ESP_LOGI(TAG, "Waiting for track to finish...");
     while (millis() - startMillis < timeoutMillis)
     {
         if (player.available() && player.readType() == DFPlayerPlayFinished)
         {
-            ESP_LOGI("Track finished");
+            ESP_LOGI(TAG, "Track finished");
             return;
         }
         delay(100);
     }
 
-    ESP_LOGE("Timed out waiting %d seconds for track to finish", timeout_seconds);
+    ESP_LOGE(TAG, "Timed out waiting %d seconds for track to finish", timeout_seconds);
 }
 
-awaitPlaybackFinished()
+void awaitPlaybackFinished()
 {
     awaitPlaybackFinished(30);
 }
@@ -62,15 +82,7 @@ extern "C" void app_main()
 {
     initArduino();
 
-    ESP_LOGI("Boot count: %d", ++bootCount);
-    if (bootCount == 1)
-    {
-        ESP_LOGI("Entering deep sleep on first boot...");
-        enterDeepSleep();
-    }
-
-    pinMode(MOSFET_PIN, OUTPUT);
-    digitalWrite(MOSFET_PIN, HIGH);
+    ESP_LOGI(TAG, "Boot count: %d", ++bootCount);
 
     Serial0.begin(115200); // Serial log
     Serial1.begin(9600);   // DFPlayer Mini
@@ -82,18 +94,21 @@ extern "C" void app_main()
     initializePlayer();
 
     int blowCount = player.readFileCountsInFolder(BLOW_FOLDER_NUM);
+    delay(100);
+    blowCount = player.readFileCountsInFolder(BLOW_FOLDER_NUM); // Read twice since first read gives wrong result
     int heinzelmannCount = player.readFileCountsInFolder(HEINZELMANN_FOLDER_NUM);
     if (blowCount <= 0 || heinzelmannCount <= 0)
     {
-        ESP_LOGE("Could not find sound samples on SD card. Restarting...");
+        ESP_LOGE(TAG, "Could not find sound samples on SD card (%d blows, %d heinzelmanns). Restarting...", blowCount, heinzelmannCount);
         ESP.restart();
     }
+    ESP_LOGI("TAG", "Found %d blow and %d heinzelmann sound samples", blowCount, heinzelmannCount);
 
-    ESP_LOGI("Playing blow...");
+    ESP_LOGI(TAG, "Playing blow...");
     player.playFolder(BLOW_FOLDER_NUM, random(1, blowCount + 1));
     awaitPlaybackFinished();
 
-    ESP_LOGI("Playing heinzelmann...");
+    ESP_LOGI(TAG, "Playing heinzelmann...");
     player.playFolder(HEINZELMANN_FOLDER_NUM, random(1, heinzelmannCount + 1));
     awaitPlaybackFinished();
 
